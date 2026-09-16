78a693c8be396fc7ee563fa1e4481f24a78caf9d
Gael Gutiérrez <gaelgutierrezdev@gmail.com>
2026-09-16 18:35:27 -0500

    feature: Implement bottom tab navigation and basic screen routes

---
+++ src/navigation/AppNavigator.tsx
@@ -0,0 +1,28 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import HomeScreen from "../screens/HomeScreen";
+import CategoriesScreen from "../screens/CategoriesScreen";
+import FavoriteScreen from "../screens/FavoriteScreen";
+
+const Tab = createBottomTabNavigator();
+
+export default function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+        />
+        <Tab.Screen
+          name="Categories"
+          component={CategoriesScreen}
+        />
+        <Tab.Screen
+          name="Favorites"
+          component={FavoriteScreen}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
