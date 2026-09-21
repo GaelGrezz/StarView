b47695c9e87e8bb9f8ab28ec69e576a2ef12be06
GaelGrezz <gutierrezgael838@gmail.com>
2026-09-20 19:40:14 -0500

    feat(ui): Implement options bar and text result

---
+++ src/constants/NasaQueries.ts
@@ -0,0 +1,30 @@
+export const NasaQueries = [
+  {
+    id: "apod",
+    label: "Foto del Día",
+    description: "Astronomy Picture of the Day",
+    searchQuery: "apod",
+    icon: "camera-outline",
+  },
+  {
+    id: "mars_rover",
+    label: "Mars Rover",
+    description: "Imágenes exploradoras de Marte",
+    searchQuery: "mars",
+    icon: "planet-outline",
+  },
+  {
+    id: "galaxies",
+    label: "Galaxias y Nebulosas",
+    description: "Capturas del espacio profundo",
+    searchQuery: "galaxy",
+    icon: "sparkles-outline",
+  },
+  {
+    id: "earth",
+    label: "Vista de la Tierra",
+    description: "Fotografías satelitales",
+    searchQuery: "earth",
+    icon: "globe-outline",
+  },
+];
--- src/screens/CategoriesScreen.tsx
+++ src/screens/CategoriesScreen.tsx
@@ -1,13 +1,56 @@
-import { View, Text, SafeAreaView } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  SafeAreaViewBase,
+  useWindowDimensions,
+} from "react-native";
 import { SafeAreaFrameContext } from "react-native-safe-area-context";
-import { HeaderComponent } from '../components/HeaderComponent';
+import { HeaderComponent } from "../components/HeaderComponent";
+import { useState } from "react";
+import { CategoryList } from "../components/CategoryList";
+import { NasaQueries } from "../constants/NasaQueries";
+import Ionicons from "@react-native-vector-icons/ionicons";
 
-export default function HomeScreen() {
+export default function CategoryScreen() {
+  const [selectedCategory, setSelectedCategory] = useState(NasaQueries[0]);
 
-    return (
+  const { height } = useWindowDimensions();
+
+  const handleSelectCategory = (category) => {
+    setSelectedCategory(category);
+    // Aquí es donde tu compañero consumirá la API de la NASA pasando el `category.searchQuery` o `category.id`
+    console.log(
+      "Categoría seleccionada para consumir API:",
+      category.searchQuery,
+    );
+  };
+
+  return (
     <SafeAreaView>
-      <View>
-        <HeaderComponent icon = "eye" />
+      <View style={{ height: height, position: "relative"}}>
+        <HeaderComponent icon="eye" />
+        <View
+          style={{
+            margin: 20,
+          }}
+        >
+          <Text style={{ fontSize: 20 }}>Resultado de: </Text>
+          <Text style={{ fontSize: 30, fontWeight: "bold" }}>
+            {selectedCategory.label}
+          </Text>
+        </View>
+        <View
+          style={{
+            position: "absolute",
+            bottom: "15%",
+          }}
+        >
+          <CategoryList
+            selectedCategory={selectedCategory}
+            onSelectCategory={handleSelectCategory}
+          />
+        </View>
       </View>
     </SafeAreaView>
   );
